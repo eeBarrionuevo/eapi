99a660cb1255f322dd6c97b20d27446162c71d3e
Elvis Barrionuevo <idcareg.g@gmail.com>
2020-03-28 22:29:18 -0500

    Generated data

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -8,5 +8,11 @@ use Faker\Generator as Faker;
 $factory->define(Product::class, function (Faker $faker) {
     return [
         //
+        'name' => $faker->word,
+        'detail' => $faker->paragraph,
+        'price' => $faker->numberBetween($min = 100, $max = 1000),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween($min = 10, $max = 100),
+        
     ];
 });
